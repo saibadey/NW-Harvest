565f182a78a56ee737558195daa1c13e439787f1
dogoodson <donnie86@gmail.com>
2016-10-15 14:04:26 -0700

    Add Address Fields

--- NWHarvest.Web/Models/Grower.cs
+++ NWHarvest.Web/Models/Grower.cs
@@ -12,7 +12,7 @@ namespace NWHarvest.Web.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Grower()
         {
-            Listings = new HashSet<Listing>();
+            PickupLocations = new HashSet<PickupLocation>();
         }
 
         public int id { get; set; }
@@ -24,10 +24,30 @@ namespace NWHarvest.Web.Models
         public string phone { get; set; }
 
         [StringLength(100)]
-        [EmailAddress]
         public string email { get; set; }
 
+        [StringLength(200)]
+        public string address1 { get; set; }
+
+        [StringLength(200)]
+        public string address2 { get; set; }
+
+        [StringLength(200)]
+        public string address3 { get; set; }
+
+        [StringLength(200)]
+        public string address4 { get; set; }
+
+        [StringLength(100)]
+        public string city { get; set; }
+
+        [StringLength(2)]
+        public string state { get; set; }
+
+        [StringLength(9)]
+        public string zip { get; set; }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Listing> Listings { get; set; }
+        public virtual ICollection<PickupLocation> PickupLocations { get; set; }
     }
 }
--- NWHarvest.Web/Models/PickupLocation.cs
+++ NWHarvest.Web/Models/PickupLocation.cs
@@ -11,10 +11,38 @@ namespace NWHarvest.Web.Models
     {
         public int id { get; set; }
 
-        public string description { get; set; }
-
-        public decimal? latlong { get; set; }
+        [StringLength(50)]
+        public string name { get; set; }
 
         public int? growerId { get; set; }
+
+        public decimal? latitude { get; set; }
+
+        public decimal? longitude { get; set; }
+
+        [StringLength(200)]
+        public string address1 { get; set; }
+
+        [StringLength(200)]
+        public string address2 { get; set; }
+
+        [StringLength(200)]
+        public string address3 { get; set; }
+
+        [StringLength(200)]
+        public string address4 { get; set; }
+
+        [StringLength(100)]
+        public string city { get; set; }
+
+        [StringLength(2)]
+        public string state { get; set; }
+
+        [StringLength(9)]
+        public string zip { get; set; }
+
+        public string comments { get; set; }
+
+        public virtual Grower Grower { get; set; }
     }
 }
