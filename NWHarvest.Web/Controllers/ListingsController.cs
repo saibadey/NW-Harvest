cff553f2c19be013d1fac6aafcfada6cce9e41cd
yabem <yabem25@gmail.com>
2016-10-15 14:36:15 -0700

    Added Listings Tab

---
+++ NWHarvest.Web/Controllers/ListingsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NWHarvest.Web.Models;
+
+namespace NWHarvest.Web.Controllers
+{
+    public class ListingsController : Controller
+    {
+        private NWHarvestEntities db = new NWHarvestEntities();
+
+        // GET: Listings
+        public ActionResult Index()
+        {
+            var listings = db.Listings.Include(l => l.Farmer);
+            return View(listings.ToList());
+        }
+
+        // GET: Listings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Listing listing = db.Listings.Find(id);
+            if (listing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(listing);
+        }
+
+        // GET: Listings/Create
+        public ActionResult Create()
+        {
+            ViewBag.listing_farmer = new SelectList(db.Farmers, "id", "name");
+            return View();
+        }
+
+        // POST: Listings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,listing_farmer,product,qtyOffered,qtyClaimed,qtyLabel,expire_date,cost,available,comments")] Listing listing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Listings.Add(listing);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.listing_farmer = new SelectList(db.Farmers, "id", "name", listing.listing_farmer);
+            return View(listing);
+        }
+
+        // GET: Listings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Listing listing = db.Listings.Find(id);
+            if (listing == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.listing_farmer = new SelectList(db.Farmers, "id", "name", listing.listing_farmer);
+            return View(listing);
+        }
+
+        // GET: Listings/Claim/5
+        public ActionResult Claim(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Listing listing = db.Listings.Find(id);
+            if (listing == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.listing_farmer = new SelectList(db.Farmers, "id", "name", listing.listing_farmer);
+            return View(listing);
+        }
+
+
+        // POST: Listings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,listing_farmer,product,qtyOffered,qtyClaimed,qtyLabel,expire_date,cost,available,comments")] Listing listing)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(listing).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.listing_farmer = new SelectList(db.Farmers, "id", "name", listing.listing_farmer);
+            return View(listing);
+        }
+
+        // POST: Listings/Claim/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Claim([Bind(Include = "id,comments")] Listing listing)
+        {
+            if (ModelState.IsValid)
+            {
+                var theComments = listing.comments;
+                listing = db.Listings.FirstOrDefault(p => p.id == listing.id);
+
+                listing.comments = theComments;
+
+                db.Entry(listing).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.listing_farmer = new SelectList(db.Farmers, "id", "name", listing.listing_farmer);
+            return View(listing);
+        }
+
+        // GET: Listings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Listing listing = db.Listings.Find(id);
+            if (listing == null)
+            {
+                return HttpNotFound();
+            }
+            return View(listing);
+        }
+
+        // POST: Listings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Listing listing = db.Listings.Find(id);
+            db.Listings.Remove(listing);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
