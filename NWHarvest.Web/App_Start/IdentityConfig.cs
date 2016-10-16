760bba4d57f87d365abc861c7e97fbd83064a85f
Michael VanderHelm <mikevh@gmail.com>
2016-10-15 18:36:46 -0700

    Reducing password complexity requirements

--- NWHarvest.Web/App_Start/IdentityConfig.cs
+++ NWHarvest.Web/App_Start/IdentityConfig.cs
@@ -83,11 +83,11 @@ namespace NWHarvest.Web
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = 4,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
