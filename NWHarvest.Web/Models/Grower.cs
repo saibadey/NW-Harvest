2ff7b1d2e18e3f8c827621b8b5440eae03517ec9
yabem <yabem25@gmail.com>
2016-10-19 14:11:58 -0700

    Added display names to Grower Pickup Location. Formatted Details page for Listings.

--- NWHarvest.Web/Models/Grower.cs
+++ NWHarvest.Web/Models/Grower.cs
@@ -25,39 +25,49 @@ namespace NWHarvest.Web.Models
         [StringLength(100)]
         public string name { get; set; }
 
+        [Display(Name = "Phone Number")]
         [StringLength(11)]
         public string phone { get; set; }
 
         [Required]
+        [Display(Name = "Email")]
         [StringLength(100)]
         public string email { get; set; }
 
         [Required]
+        [Display(Name = "Address 1")]
         [StringLength(200)]
         public string address1 { get; set; }
 
+        [Display(Name = "Address 2")]
         [StringLength(200)]
         public string address2 { get; set; }
 
+        [Display(Name = "Address 3")]
         [StringLength(200)]
         public string address3 { get; set; }
 
+        [Display(Name = "Address 4")]
         [StringLength(200)]
         public string address4 { get; set; }
 
         [Required]
+        [Display(Name = "City")]
         [StringLength(100)]
         public string city { get; set; }
 
         [Required]
+        [Display(Name = "State")]
         [StringLength(2)]
         public string state { get; set; }
 
         [Required]
+        [Display(Name = "Zip")]
         [StringLength(9)]
         public string zip { get; set; }
 
         [Required]
+        [Display(Name = "Notification Preference")]
         public string NotificationPreference { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
